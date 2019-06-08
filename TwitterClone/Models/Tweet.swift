29b2b4ecb7a7c16191f43c81a558b5244943bed8
masatsch <masato.11.soccer@gmail.com>
2019-06-08 17:06:51 +0900

    model

---
+++ TwitterClone/Models/Tweet.swift
@@ -0,0 +1,25 @@
+//
+//  SwiftUIView.swift
+//  TwitterClone
+//
+//  Created by Masato TSUTSUMI on 2019/06/08.
+//  Copyright © 2019 Masato TSUTSUMI. All rights reserved.
+//
+
+import SwiftUI
+import CoreLocation
+
+struct Tweet: Hashable, Codable, Identifiable {
+    var id: Int
+    var text: String
+    var user: User
+    fileprivate var imageName: String
+    var isFavorite: Bool
+    var isRetweeted: Bool
+    
+    func image(forSize size: Int) -> Image {
+        ImageStore.shared.image(name: imageName, size: size)
+    }
+    
+    
+}
