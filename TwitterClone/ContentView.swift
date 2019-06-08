99ae5e32e8baf17a70b8a29d45f9a7a3965bedda
masatsch <masato.11.soccer@gmail.com>
2019-06-08 16:54:31 +0900

    Initial Commit

---
+++ TwitterClone/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  TwitterClone
+//
+//  Created by Masato TSUTSUMI on 2019/06/08.
+//  Copyright © 2019 Masato TSUTSUMI. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ TwitterCloneTests/TwitterCloneTests.swift
@@ -0,0 +1,34 @@
+//
+//  TwitterCloneTests.swift
+//  TwitterCloneTests
+//
+//  Created by Masato TSUTSUMI on 2019/06/08.
+//  Copyright © 2019 Masato TSUTSUMI. All rights reserved.
+//
+
+import XCTest
+@testable import TwitterClone
+
+class TwitterCloneTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
